21db565f6284500aacec151c92caa7a6dd491fa6
Matthew <99067338@mydavinci.nl>
2021-10-28 10:12:16 +0200

    added 3 flavours

--- Papi-Gelatol.py
+++ Papi-Gelatol.py
@@ -9,6 +9,14 @@ def bestellen():
     while x == True:
         aantalBolletjes = int(str(input('Hoeveel bolletjes wil je? ')))
         if aantalBolletjes in [1,2,3]:
+            y = 0
+            while y < aantalBolletjes:
+                y += 1
+                smaak =  input ('Welke smaak wilt u voor bolletje nummer '+ str(y) + '? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? ')
+                if smaak.upper() in "ACMV": print () 
+                else: 
+                    print ('Sorry, dat snap ik niet...')  
+                    y -= 1   
             hoorentjeofbakje = input('Wilt u deze '+str(aantalBolletjes) +' bolletje(s) in A) een hoorntje of B) een bakje?” ').upper()
             if hoorentjeofbakje == ('A'):
                 hoorentjeofbakje = 'hoorntje'
@@ -16,13 +24,20 @@ def bestellen():
             elif hoorentjeofbakje == ('B'):
                 hoorentjeofbakje = ('bakje')
                 print ('Hier is uw '+ (hoorentjeofbakje)+' met '+ str(aantalBolletjes) +' bolletje(s)')
+                    
                 
-            
-                
+                    
             else: print ('Sorry, dat snap ik niet...') 
 
         elif aantalBolletjes in [4,5,6,7,8]:
-            print ('Dan krijgt u van mij een bakje met ' + str(aantalBolletjes) +' bolletjes')
+            y = 0
+            while y < aantalBolletjes:
+                y += 1
+                smaak =  input ('Welke smaak wilt u voor bolletje nummer '+ str(y) + '? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? ')
+                if smaak.upper() in "ACMV": print () 
+                else: 
+                    print ('Sorry, dat snap ik niet...')  
+                    y -= 1  
         elif aantalBolletjes > int((8)):
             print('Sorry zulke grote bakken hebben we niet')
             bestellen()
@@ -38,7 +53,6 @@ def bestellen():
                 x = False
                 o = False
             else: print ('Sorry, dat snap ik niet') 
-
         return aantalBolletjes
 
 
