9cf43c8606f4b4c52b5676c3bb9f45ab02b4fc52
Matthew <99067338@mydavinci.nl>
2021-11-03 09:54:55 +0100

    bon is klaar

--- Papi-Gelatol.py
+++ Papi-Gelatol.py
@@ -1,12 +1,33 @@
 print ('Welkom bij Papi Gelato')
 
 
-prijshoorentje = 1,25
-prijsbakje = 0,75
-prijsbolletjes = 1,25
+prijshoorentje = 1.25
+prijsbakje = 0.75
+prijsbolletjes = 1.25
+aantalBolletjes = 0
+totaalAantalBolletjes = 0
+hoorentjeofbakje = ''
+totaalAantalHoorentjes = 0
+totaalAantalBakjes = 0
+totaal = 0
 
+
+def bonnentje():
+    global aantalBolletjes, hoorentjeofbakje, totaalAantalBolletjes, totaalAantalHoorentjes, totaalAantalBakjes
+    totaal = (totaalAantalBolletjes * prijsbolletjes) + (totaalAantalHoorentjes * prijshoorentje) + (totaalAantalBakjes * prijsbakje)
+    print ('--------------["Papi Gelato"]--------------')
+    print ('Bedankt voor je bestelling hierbij uw bon!')
+    if aantalBolletjes > 0:
+        print (f'bolletjes: {totaalAantalBolletjes} x  {prijsbolletjes} = € ' + str(totaalAantalBolletjes * prijsbolletjes))
+    if totaalAantalHoorentjes > 0:
+        print (f'Hoorentje: {totaalAantalHoorentjes} x {prijshoorentje} = € '+ str(totaalAantalHoorentjes * prijshoorentje) +'')
+    if totaalAantalBakjes > 0:
+        print (f'Bakje: {totaalAantalBakjes} x {prijsbakje} = € '+ str(totaalAantalBakjes * prijsbakje) +'')
+    print (f'Totaal: € {totaal}')
+    print ('--------------["Papi Gelato"]--------------')
 
 def bestellen():
+    global aantalBolletjes, hoorentjeofbakje, totaalAantalBolletjes, totaalAantalHoorentjes, totaalAantalBakjes
     x = True
     while x == True:
         aantalBolletjes = int(str(input('Hoeveel bolletjes wil je? ')))
@@ -21,11 +42,13 @@ def bestellen():
                     y -= 1   
             hoorentjeofbakje = input('Wilt u deze '+str(aantalBolletjes) +' bolletje(s) in A) een hoorntje of B) een bakje?” ').upper()
             if hoorentjeofbakje == ('A'):
-                hoorentjeofbakje = 'hoorntje'
-                print ('Hier is uw '+ (hoorentjeofbakje)+' met '+ str(aantalBolletjes) +' bolletje(s)')
+                hoorentjeofbakje = 'A'
+                totaalAantalHoorentjes += 1
+                print ('Hier is uw hoorentje met '+ str(aantalBolletjes) +' bolletje(s)')
             elif hoorentjeofbakje == ('B'):
-                hoorentjeofbakje = ('bakje')
-                print ('Hier is uw '+ (hoorentjeofbakje)+' met '+ str(aantalBolletjes) +' bolletje(s)')
+                hoorentjeofbakje = ('B')
+                totaalAantalBakjes += 1
+                print ('Hier is uw bakje met ' + str(aantalBolletjes) +' bolletje(s)')
                     
                 
                     
@@ -33,6 +56,7 @@ def bestellen():
 
         elif aantalBolletjes in [4,5,6,7,8]:
             y = 0
+            hoorentjeofbakje = "B"
             while y < aantalBolletjes:
                 y += 1
                 smaak =  input ('Welke smaak wilt u voor bolletje nummer '+ str(y) + '? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? ')
@@ -47,35 +71,29 @@ def bestellen():
         o = True
         while o == True:
             opnieuwbestellen = input ('Wil je op nieuw bestellen Ja (J) of Nee (N)').upper()
-            if opnieuwbestellen == ('J'):
+            totaalAantalBolletjes += aantalBolletjes
+            if opnieuwbestellen == ('J'):              
                 bestellen()
                 o = False
             elif opnieuwbestellen == ('N'):
                 print ('Bedankt en tot ziens!')
                 x = False
                 o = False
+                bonnentje()
             else: print ('Sorry, dat snap ik niet') 
         return aantalBolletjes, hoorentjeofbakje
 
 
 
 
-def bonnentje(hoorentjeofbakje,aantalBolletjes ):
-    print ('--------------["Papi Gelato"]--------------')
-    print ('Bedankt voor je bestelling hierbij uw bon!')
-    if aantalBolletjes > ('0'):
-        print ('bolletjes:' + aantalBolletjes * prijsbolletjes)
-    if hoorentjeofbakje == ('A'):
-        print ('Hoorentje: '+ prijshoorentje +'')
-    if hoorentjeofbakje == ('B'):
-        print ('Bakje: '+ prijsbakje +'')
+
 
 
 
 
 
 bestellen() 
-bonnentje()
+
 
 
 
