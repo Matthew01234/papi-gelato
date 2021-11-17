31ac343529c7f94d077bc50be547eeb4285f4b56
Matthew <99067338@mydavinci.nl>
2021-11-17 11:18:44 +0100

    top ping toegevoegd

--- Papi-Gelatol.py
+++ Papi-Gelatol.py
@@ -1,24 +1,35 @@
 print ('Welkom bij Papi Gelato')
 
+hoorentjeofbakje = ''
 
 prijshoorentje = 1.25
+totaalAantalHoorentjes = 0
+
 prijsbakje = 0.75
+totaalAantalBakjes = 0
+
 prijsbolletjes = 1.25
-prijsSlagroom = 0,5
-prijsSprinkels = 0,3
-prijsCaramel = ''
 aantalBolletjes = 0
 totaalAantalBolletjes = 0
-hoorentjeofbakje = ''
-totaalAantalHoorentjes = 0
-totaalAantalBakjes = 0
+
+prijsSlagroom = 0.5
+prijsSprinkels = 0.3
+prijsCaramelhorentje = 0.0
+prijsCaramelbakje = 0.0
+
+totaalAantalSlagroom = 0
+totaalAantalSprinkels = 0
+totaalAantalCaramel = 0 
+totaalAantalTopping = 0
 totaal = 0
 
 
 
+
+
 def bonnentje():
-    global aantalBolletjes, hoorentjeofbakje, totaalAantalBolletjes, totaalAantalHoorentjes, totaalAantalBakjes
-    totaal = (totaalAantalBolletjes * prijsbolletjes) + (totaalAantalHoorentjes * prijshoorentje) + (totaalAantalBakjes * prijsbakje)
+    global aantalBolletjes, hoorentjeofbakje, totaalAantalBolletjes, totaalAantalHoorentjes, totaalAantalBakjes,totaalAantalSlagroom, prijsSlagroom,prijsSprinkels,totaalAantalSprinkels
+    totaal = (totaalAantalBolletjes * prijsbolletjes) + (totaalAantalHoorentjes * prijshoorentje) + (totaalAantalBakjes * prijsbakje) + totaalAantalSlagroom * prijsSlagroom + totaalAantalSprinkels * prijsSprinkels * aantalBolletjes +prijsCaramelhorentje + prijsCaramelbakje
     print ('--------------["Papi Gelato"]--------------')
     print ('Bedankt voor je bestelling hierbij uw bon!')
     if aantalBolletjes > 0:
@@ -27,11 +38,34 @@ def bonnentje():
         print (f'Hoorentje: {totaalAantalHoorentjes} x {prijshoorentje} = € '+ str(totaalAantalHoorentjes * prijshoorentje) +'')
     if totaalAantalBakjes > 0:
         print (f'Bakje: {totaalAantalBakjes} x {prijsbakje} = € '+ str(totaalAantalBakjes * prijsbakje) +'')
+    if totaalAantalTopping > 0:
+        print (f'topping: {totaalAantalTopping} =    €'+ str(totaalAantalSlagroom * prijsSlagroom + totaalAantalSprinkels * prijsSprinkels * aantalBolletjes +prijsCaramelhorentje + prijsCaramelbakje) + '')
     print (f'Totaal: € {totaal}')
     print ('--------------["Papi Gelato"]--------------')
 
+def kiesTopping():
+    global totaalAantalSlagroom, totaalAantalSprinkels, totaalAantalCaramel, totaalAantalTopping
+    topping = input ('Wat voor topping wilt u: A) Geen, B) Slagroom, C) Sprinkels of D) Caramel Saus?: ').upper()
+    if topping in ['A','B','C','D']:
+        if topping == ('A'):
+            print ('')
+
+        if topping ==('B'):
+            totaalAantalSlagroom += 1
+            totaalAantalTopping += 1
+
+        if topping ==('C'):
+            totaalAantalSprinkels += 1
+            totaalAantalTopping += 1
+
+        if topping ==('D'):
+            totaalAantalCaramel += 1
+            totaalAantalTopping += 1
+        
+
+
 def bestellen():
-    global aantalBolletjes, hoorentjeofbakje, totaalAantalBolletjes, totaalAantalHoorentjes, totaalAantalBakjes
+    global aantalBolletjes, hoorentjeofbakje, totaalAantalBolletjes, totaalAantalHoorentjes, totaalAantalBakjes, prijsCaramelhorentje, prijsCaramelbakje
     x = True
     while x == True:
         aantalBolletjes = int(str(input('Hoeveel bolletjes wil je? ')))
@@ -39,28 +73,34 @@ def bestellen():
             y = 0
             while y < aantalBolletjes:
                 y += 1
-                topping = input ('Welke topping wil je ')
                 smaak =  input ('Welke smaak wilt u voor bolletje nummer '+ str(y) + '? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? ')
-                if smaak.upper() in "ACMV": print () 
+                if smaak.upper() in     "ACMV": print () 
                 else: 
                     print ('Sorry, dat snap ik niet...')  
                     y -= 1   
+                
+            kiesTopping()
             hoorentjeofbakje = input('Wilt u deze '+str(aantalBolletjes) +' bolletje(s) in A) een hoorntje of B) een bakje?” ').upper()
             if hoorentjeofbakje == ('A'):
                 hoorentjeofbakje = 'A'
                 totaalAantalHoorentjes += 1
+                prijsCaramelhorentje += 0.6
                 print ('Hier is uw hoorentje met '+ str(aantalBolletjes) +' bolletje(s)')
+                
             elif hoorentjeofbakje == ('B'):
                 hoorentjeofbakje = ('B')
                 totaalAantalBakjes += 1
+                prijsCaramelbakje += 0.9
                 print ('Hier is uw bakje met ' + str(aantalBolletjes) +' bolletje(s)')
-                    
                 
+              
                     
             else: print ('Sorry, dat snap ik niet...') 
 
         elif aantalBolletjes in [4,5,6,7,8]:
             y = 0
+            totaalAantalBakjes += 1
+            prijsCaramelbakje += 0.9
             hoorentjeofbakje = "B"
             while y < aantalBolletjes:
                 y += 1
@@ -69,6 +109,7 @@ def bestellen():
                 else: 
                     print ('Sorry, dat snap ik niet...')  
                     y -= 1  
+            kiesTopping()
         elif aantalBolletjes > int((8)):
             print('Sorry zulke grote bakken hebben we niet')
             bestellen()
